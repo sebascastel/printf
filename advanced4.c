a92a8c7551ae67444ee8790721eb20a5c196c9b4
sebascastel <770@holbertonschool.com>
2019-03-15 20:36:10 +0000

    add octal

--- advanced4.c
+++ advanced4.c
@@ -21,3 +21,42 @@ int u(va_list unsi)
 	}
 	return (a);
 }
+/**
+ *o - octal
+ *@oct: octal
+ *Return: o
+ */
+int o(va_list oct)
+{
+	unsigned int a;
+	unsigned int b;
+	int *c;
+	int d;
+	int e = 0;
+
+	a = va_arg(oct, unsigned int);
+	b = a;
+	while (a / 8 != 0)
+	{
+	a /= 8;
+	e++;
+	}
+	e++;
+	c = malloc(sizeof(int) * e);
+	if (c == NULL)
+	{
+	free(c);
+	return (0);
+	}
+	for (d = 0; d < e; d++)
+	{
+	c[d] = b % 8;
+	b /= 8;
+	}
+	for (d = e - 1; d >= 0; d--)
+	{
+	_putchar(c[d] + '0');
+	}
+	free(c);
+	return (e);
+}
