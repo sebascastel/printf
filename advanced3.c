db492de7afff7f092591753a8619a9e9fbd185e3
sebascastel <770@holbertonschool.com>
2019-03-15 19:56:58 +0000

    add binary

---
+++ advanced3.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ *b - return binary
+ *@bin: binary
+ *Return: binary
+ */
+int b(va_list bin)
+{
+	unsigned int a;
+	unsigned int b;
+	int *c;
+	int d;
+	int e = 0;
+
+	a = va_arg(bin, unsigned int);
+	b = a;
+	while (a / 2 != 0)
+	{
+	a /= 2;
+	e++;
+	}
+	e++;
+	c = malloc(sizeof(int) * e);
+	if (c == NULL)
+	{
+	free(c);
+	return (0);
+	}
+	for (d = 0; d < e; d++)
+	{
+	c[d] = b % 2;
+	b /= 2;
+	}
+	for (d = e - 1; d >= 0; d--)
+	{
+	_putchar(c[d] + '0');
+	}
+	free(c);
+	return (e);
+}
